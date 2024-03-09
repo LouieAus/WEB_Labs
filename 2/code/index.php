44c6614687c49368e3eedcdb9aa5a2f0c8ea0460
Lvov <i.lvov81@mail.ru>
2024-03-09 13:15:59 +0200

    Completed ex. 11

--- 2/code/index.php
+++ 2/code/index.php
@@ -23,3 +23,11 @@ echo --$var1, "\n";
 $last_month = 1187.23;
 $this_month = 1089.98;
 echo $last_month - $this_month, "\n";
+
+
+// ======== Задание 11 ========
+$num_languages = 4;
+$months = 11;
+$days = $months * 16;
+$days_per_languages = $days / $num_languages;
+echo $days_per_languages, "\n";
