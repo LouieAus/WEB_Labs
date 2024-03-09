ba042db24257199ce8d90ae66490a36b3307cbcc
Lvov <i.lvov81@mail.ru>
2024-03-09 13:15:14 +0200

    Completed ex. 1

--- 2/code/index.php
+++ 2/code/index.php
@@ -1,2 +1,11 @@
 <?php
-echo phpinfo();
+header('Content-type: text/plain');
+
+
+// ======== Задание 1 ========
+$veryBadUnclearName = "data info ";
+
+$order =& $veryBadUnclearName;
+$order .= "user1";
+
+echo "Developer variable is $veryBadUnclearName\n";
