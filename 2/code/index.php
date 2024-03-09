5d9dca9cf291a2aea81cb6b99c94991d7285616d
Lvov <i.lvov81@mail.ru>
2024-03-09 13:15:40 +0200

    Completed ex. 2

--- 2/code/index.php
+++ 2/code/index.php
@@ -9,3 +9,17 @@ $order =& $veryBadUnclearName;
 $order .= "user1";
 
 echo "Developer variable is $veryBadUnclearName\n";
+
+
+// ======== Задание 2 ========
+$var1 = 13;
+echo "$var1\n";
+
+$var2 = 3.14;
+echo "$var2\n";
+
+echo --$var1, "\n";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo $last_month - $this_month, "\n";
