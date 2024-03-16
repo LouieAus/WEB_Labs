c36d2bd91362d8804dafd5f66643eb73db3eaa0e
Lvov <i.lvov81@mail.ru>
2024-03-16 19:21:27 +0200

    Added Ex. 1

--- 3/code/index1.php
+++ 3/code/index1.php
@@ -1,4 +1,6 @@
 <?php
+header('Content-type: text/plain');
+
 // ====== Задание 1.a ======
 $text = 'ahb acb aeb aeeb adcb axeb';
 $regexp = '/a[a-z][a-z]b/ui';
@@ -8,3 +10,14 @@ $count = preg_match_all($regexp, $text, $matches);
 var_dump($matches);
 
 // ====== Задание 1.b ======
+$text = 'a1b2c3';
+$regexp = '/[0-9]+/';
+
+$result = preg_replace_callback(
+    $regexp,
+    function ($num)
+    {
+        return (string)(pow($num[0], 3));
+    },
+    $text);
+echo "\n", $result;
